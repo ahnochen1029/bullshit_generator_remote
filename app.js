08f6d5be182430d96cf0a41298c072745b078d24
Ahno Chen <ahnochen1029@gmail.com>
2020-08-27 00:43:12 +0800

    add bodyParser

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const app = express()
 const exphds = require('express-handlebars')
+const bodyParser = require('body-parser')
+const generateTrash = require('./generator')
 const port = 3000
 
 // setting template engine
 app.engine('handlebars', exphds({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const trash = generateTrash(req.body)
+  res.render('index', { trash: trash })
 })
 
 //setting static files
